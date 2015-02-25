fcae505b63a77a17cc8e644bc4fd441507be4c36
uroboro <uroboro845@gmail.com>
2015-02-24 23:02:53 -0300

    Bundle-ified bundle name fetching

--- flipnc/FlipNCListController.mm
+++ flipnc/FlipNCListController.mm
@@ -8,20 +8,12 @@
 #endif
 #define showAlert(t, m) [[[[objc_getClass("UIAlertView") alloc] initWithTitle:(t) message:(m) delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] autorelease] show];
 
-#define CMCLog(format, ...) \
-	NSLog(@"\033[1;36m(%s) in [%s:%d]\033[0m \033[5;32;40m:::\033[0m \033[0;31m%@\033[0m", __PRETTY_FUNCTION__, __FILE__, __LINE__, [NSString stringWithFormat:format, ## __VA_ARGS__])
-
-#define CMCLogObjDescript(a) \
-	CMCLog(@"flipnc :: %s=%@", #a, [a description])
-
 static NSString *templatesPath = @"/Library/Application Support/FlipNC";
 
 @interface FlipNCListController: PSListController {
 	NSMutableArray *_values;
 	NSMutableArray *_titles;
 }
-
-//- (PSSpecifier *)templateListSpecifier;
 
 @end
 
@@ -61,11 +53,10 @@ static NSString *templatesPath = @"/Library/Application Support/FlipNC";
 	//for each unique value, try to get the CFBundleName of its corresponding Info.plist. Default to the value
 	NSMutableArray *titles = [NSMutableArray new];
 	for (NSString *val in [self templateValues]) {
-		NSString *InfoPlistPath = [NSString stringWithFormat:@"%@/%@%s.bundle/Info.plist", templatesPath, val, isPad()?"~ipad":""];
-		NSDictionary *dict = [[NSDictionary alloc] initWithContentsOfFile:InfoPlistPath];
-		NSString *name = [dict objectForKey:@"CFBundleName"];
+		NSString *path = [NSString stringWithFormat:@"%@/%@%s.bundle", templatesPath, val, isPad()?"~ipad":""];
+		NSBundle *bundle = [NSBundle bundleWithPath:path];
+		NSString *name = [bundle objectForInfoDictionaryKey:@"CFBundleDisplayName"];
 		[titles addObject:(name)?name:val];
-		[dict release];
 	}
 	_titles = [titles mutableCopy];
 	[titles release];
